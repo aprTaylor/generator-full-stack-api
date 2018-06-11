4be782862092b0b9de00f328610db17920980af0
Angelo Manganiello <angelo.mang@libero.it>
2018-06-11 12:58:01 +0200

    change api for mongo

--- example/server/config/database.js
+++ example/server/config/database.js
@@ -1,7 +1,15 @@
 const database = {
-  mongo: {
-    url: process.env.MONGO_DB_URI || 'mongodb://localhost:27017/example'
-  }
+	mongo: {
+		url: getDbUrl()
+	}
 };
+
+function getDbUrl() {
+
+	if (process.env.MONGO_EMBEDDED === 'true')
+		return 'mongodb://' + __dirname;
+
+	return process.env.MONGO_DB_URI || 'mongodb://localhost:27017/example'
+}
 
 module.exports = database;
--- generators/app/templates/server/lib/controller.js
+++ generators/app/templates/server/lib/controller.js
@@ -1,53 +1,83 @@
 class Controller {
-  constructor(facade) {
-    this.facade = facade;
-  }
+	constructor(facade) {
+		this.facade = facade;
+	}
 
-  create(req, res, next) {
-    this.facade.create(req.body)
-      .then(doc => res.status(201).json(doc))
-      .catch(err => next(err));
-  }
+	create(req, res, next) {
+		return this.facade.create(req.body)
+		.save(function (err, doc) {
+			if (err)
+				return next(err);
+			res.status(201).json(doc)
 
-  find(req, res, next) {
-    return this.facade.find(req.query)
-      .then(collection => res.status(200).json(collection))
-      .catch(err => next(err));
-  }
+		});
+	}
 
-  findOne(req, res, next) {
-    return this.facade.findOne(req.query)
-      .then(doc => res.status(200).json(doc))
-      .catch(err => next(err));
-  }
+	find(req, res, next) {
+		return this.facade.getModel()
+		.find(req.query, function (err, collection) {
+			if (err)
+				return next(err);
+			res.status(200).json(collection)
+		})
 
-  findById(req, res, next) {
-    return this.facade.findById(req.params.id)
-      .then((doc) => {
-        if (!doc) { return res.sendStatus(404); }
-        return res.status(200).json(doc);
-      })
-      .catch(err => next(err));
-  }
+	}
 
-  update(req, res, next) {
-    this.facade.update({ _id: req.params.id }, req.body)
-      .then((results) => {
-        if (results.n < 1) { return res.sendStatus(404); }
-        if (results.nModified < 1) { return res.sendStatus(304); }
-        res.sendStatus(204);
-      })
-      .catch(err => next(err));
-  }
+	findOne(req, res, next) {
+		return this.facade.getModel()
+		.findOne(req.query, function (err, doc) {
+			if (err)
+				return next(err);
+			res.status(200).json(doc)
+		})
+	}
 
-  remove(req, res, next) {
-    this.facade.remove({ _id: req.params.id })
-      .then((doc) => {
-        if (!doc) { return res.sendStatus(404); }
-        return res.sendStatus(204);
-      })
-      .catch(err => next(err));
-  }
+	findById(req, res, next) {
+		return this.facade.getModel().findById(req.params.id, function (err, doc) {
+			if (!doc) {
+				return res.sendStatus(404);
+			}
+			if (err)
+				return next(err);
+
+			return res.status(200).json(doc);
+
+		})
+
+	}
+
+	update(req, res, next) {
+		this.facade.getModel().update({
+			_id: req.params.id
+		}, req.body, function (err, results) {
+			if (err)
+				return next(err);
+
+			if (results.n < 1) {
+				return res.sendStatus(404);
+			}
+			if (results.nModified < 1) {
+				return res.sendStatus(304);
+			}
+			res.sendStatus(204);
+
+		})
+	}
+
+	remove(req, res, next) {
+		this.facade.getModel().remove({
+			_id: req.params.id
+		}, function (err, doc) {
+			if (!doc) {
+				return res.sendStatus(404);
+			}
+			if (err)
+				return next(err);
+
+			return res.sendStatus(204);
+
+		})
+	}
 }
 
 module.exports = Controller;
